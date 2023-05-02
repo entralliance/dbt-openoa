e13cb8e174eae40b21c3fc2cf2c552534a0bef18
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-05-02 07:13:37 -0400

    checkpoint for postgres type casting issue

--- models/intermediate/int_openoa_wtg_scada__filtered.sql
+++ models/intermediate/int_openoa_wtg_scada__filtered.sql
@@ -35,6 +35,11 @@ energy_union as (
 
     select * from power_to_energy
 ),
+            {# 'case
+                when mod( ( mod(tag_value, 360) + 360 ), 360 ) > 180 then 
+                     mod( ( mod(tag_value, 360) + 360 ), 360 ) - 360
+                else mod( ( mod(tag_value, 360), 360 ), 360 )
+            end', #}
 
 unit_conversions as (
     {{entr_multiple_tag_unit_conversions(
@@ -45,7 +50,7 @@ unit_conversions as (
             2378
         ],
         operations=[
-            'case when (mod(mod(tag_value, 360) + 360), 360) > 180 then (mod( mod(tag_value, 360) + 360 ), 360) - 360 else (mod(mod(tag_value, 360) + 360), 360) end',
+            'case when ((tag_value % 360) + 360) % 360 > 180 then ((tag_value % 360) + 360) % 360 - 360 else ((tag_value % 360) + 360) % 360 end',
             'tag_value * 1000',
             'tag_value * 1000'
         ],
