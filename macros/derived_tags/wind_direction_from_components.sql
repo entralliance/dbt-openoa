20d8b75e47f98eb621a67c9c93726276fc0ad902
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-05-02 05:53:44 -0400

    handle pi for bigquery

--- macros/derived_tags/wind_direction_from_components.sql
+++ macros/derived_tags/wind_direction_from_components.sql
@@ -5,3 +5,7 @@
 {% macro default__wind_direction_from_components(u, v) %}
     180 + atan2({{u}}, {{v}}) * 180 / pi()
 {% endmacro %}
+
+{% macro bigquery__wind_direction_from_components(u, v) %}
+    180 + atan2({{u}}, {{v}}) * 180 / acos(-1)
+{% endmacro %}
